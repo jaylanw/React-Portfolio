6dca8bc70bfe70da5cd6dfda6b95b488e0adedc1
Jaylan <jaylanwienckowski@gmail.com>
2020-09-08 10:37:53 -0500

    updated gallery

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Nav from './components/Nav'
 // import Contact from './components/Contact'
 // import Web from './components/Web'
-// import Gallery from './components/Gallery'
-import Content from './components/Content'; 
+ import Gallery from './components/Gallery'
+// import Content from './components/Content'; 
 
 function App() {
   return (
       <div>
         <Nav />
-        <Content />
+        <Gallery />
         
       </div>
   );
