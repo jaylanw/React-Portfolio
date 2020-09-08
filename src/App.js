0349e7a68244749b754c555f21f591e64d11ec6f
Jaylan <jaylanwienckowski@gmail.com>
2020-09-08 10:23:41 -0500

    added content

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Nav from './components/Nav'
 // import Contact from './components/Contact'
 // import Web from './components/Web'
-import Gallery from './components/Gallery'
+// import Gallery from './components/Gallery'
+import Content from './components/Content'; 
+
 function App() {
   return (
       <div>
         <Nav />
-        <Gallery />
+        <Content />
         
       </div>
   );
---
+++ src/components/Content/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.css'
+function Content() {
+    return (
+    <div className="container">
+<main className="site-content">
+    <div className="row">
+      <div className="col-sm-6 col-md-3">
+        <img src="assets/content/AML1.jpg" alt="content creation" />
+      </div>
+      <div className="col-sm-6 col-md-3">
+        <img src="assets/content/content1.jpeg" alt="content creation"  />
+        <img src="assets/content/daughters1.jpg" alt="content creation" />
+      </div>
+      <div className="col-sm-6 col-md-3">
+        <img src="assets/content/praise.jpg" alt="content creation" />
+        <img src="assets/content/content2.jpeg" alt="content creation" />
+      </div>
+      <div className="col-sm-6 col-md-3">
+        <img src="assets/content/OpenDoor.jpg" alt="content creation" />
+      </div>
+    </div>
+</main>
+</div>
+
+    )
+}
+
+export default Content;
